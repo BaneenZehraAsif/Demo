53ba004e80e164a8d64f657bab3427dfce90d380
BaneenZehraASif <baneenzehra47@gmail.com>
2025-02-09 13:57:24 +0500

    meny code template hata di

--- main.py
+++ main.py
@@ -37,15 +37,3 @@
 
 
 # 
-#
-from pytube import YouTube
-
-url = "https://www.youtube.com/watch?v=ZTrrc6Ni5eM"
-
-try:
-   video = YouTube(url)
-   stream = video.streams.filter(only_audio=True).first()
-   stream.download(filename=f"{video.title}.mp3")
-   print("The video is downloaded in MP3")
-except KeyError:
-   print("Unable to fetch video information. Please check the video URL or your network connection.")
